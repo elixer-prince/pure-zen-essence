361e65c9072b4d97452187d9fc80057b15f091b0
Daniel Leith <elixerprince@gmail.com>
2025-05-27 12:54:39 -0500

    Refactor AboutSection layout and styling; update image source and heading text

--- frontend/src/components/AboutSection.tsx
+++ frontend/src/components/AboutSection.tsx
@@ -3,13 +3,13 @@ const AboutSection = () => {
     <section
       id="about"
       aria-labelledby="about-header"
-      className="relative grid scroll-mt-24 grid-cols-1 gap-16 bg-linear-to-r from-neutral-950 to-neutral-900 p-16 text-white md:grid-cols-2"
+      className="relative grid scroll-mt-24 grid-cols-1 gap-16 bg-linear-to-r from-neutral-950 to-neutral-900 p-16 pt-12 text-white md:grid-cols-2"
     >
       {/* Left Container */}
       <div>
         <img
-          className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-[50%] flex aspect-square min-h-[calc(100dvh-6rem] w-1/2 items-center justify-center rounded-full border-4 border-white bg-amber-700 md:static"
-          src=""
+          className="absolute top-0 left-1/2 flex aspect-square min-h-[calc(100dvh-6rem] w-[12rem] max-w-[100%] -translate-x-1/2 -translate-y-[50%] items-center justify-center rounded-full border-4 border-white md:static"
+          src="https://images.unsplash.com/photo-1536147210925-5cb7a7a4f9fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JlZW4lMjBwbGFudHxlbnwwfHwwfHx8MA%3D%3D"
           alt="Image goes here..."
         />
       </div>
@@ -20,11 +20,11 @@ const AboutSection = () => {
             id="about-header"
             className="border-brand-green border-b-8 pb-4 text-center text-4xl font-black"
           >
-            ABOUT US
+            ABOUT
           </h2>
         </header>
         <div>
-          <h3 className="mt-8 mb-4 text-center text-4xl font-bold">
+          <h3 className="border-brand-blue mt-8 mb-4 border-l-8 pb-2 pl-4 text-4xl font-bold">
             Our Mission
           </h3>
           <p>
@@ -36,7 +36,7 @@ const AboutSection = () => {
           </p>
         </div>
         <div>
-          <h3 className="mt-8 mb-4 text-center text-4xl font-bold">
+          <h3 className="border-brand-blue mt-8 mb-4 border-l-8 pb-2 pl-4 text-4xl font-bold">
             Our Vision
           </h3>
           <p>
