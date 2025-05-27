d303d3b1286b7d7e34f94b20f8294a770668bb35
Daniel Leith <elixerprince@gmail.com>
2025-05-27 09:42:52 -0500

    Refactor Netlify configuration and improve type definitions in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 // Data
 import products from "./products.ts";
+import type { Product } from "./products.ts";
 // Pages
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage.tsx";
 
-export const ProductsContext = createContext([{}]);
+export type { Product };
+
+export const ProductsContext = createContext<Product[]>([{} as Product]);
 
 const App = () => {
   return (
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,7 +20,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+      "@": "/src"
     }
   }
 });
