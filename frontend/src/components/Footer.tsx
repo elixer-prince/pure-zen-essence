361d92d9b2fee087c0a902c306dc8618f8930c66
Daniel Leith <elixerprince@gmail.com>
2025-05-27 01:58:52 -0500

    Update Footer content

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,17 @@
 const Footer = () => {
-  return <footer className="border-2 border-amber-400">Footer</footer>;
+  return (
+    <footer className="border-2 border-amber-400">
+      <ul>
+        <li>
+          <a href="">Instagram</a>
+        </li>
+        <li>
+          <a href="">TikTok</a>
+        </li>
+      </ul>
+      <p>&copy; 2025 Pure Zen Essence</p>
+    </footer>
+  );
 };
 
 export default Footer;
