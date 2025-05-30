ac24bec38616139c2edb3aafe7552fcbd9b89e91
Daniel Leith <elixerprince@gmail.com>
2025-05-29 23:22:01 -0500

    Enhance Footer layout; adjust structure and styling for improved usability and visual appeal

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,29 @@
+import { FaInstagram, FaTiktok } from 'react-icons/fa';
+
 const Footer = () => {
   return (
-    <footer className="bg-black px-8 py-16 text-center text-white">
-      <ul className="flex justify-center gap-4 text-lg">
-        <li>
-          <a href="">Instagram</a>
+    <footer className="bg-black px-4 sm:px-8 py-8 sm:py-16 text-center text-white">
+      <ul className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-8">
+        <li className="w-full sm:w-auto">
+          <a 
+            href="https://www.instagram.com" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="text-4xl hover:text-brand-green transition-colors duration-300" />
+          </a>
         </li>
-        <li>
-          <a href="">TikTok</a>
+        <li className="w-full sm:w-auto">
+          <a 
+            href="https://www.tiktok.com"
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <FaTiktok className="text-4xl hover:text-brand-green transition-colors duration-300" />
+          </a>
         </li>
       </ul>
-      <p className="mt-8 text-sm">Copyright &copy; 2025 Pure Zen Essence</p>
+      <p className="mt-4 sm:mt-8 sm:text-sm md:text-md lg:text-lg">Copyright &copy; 2025 Pure Zen Essence</p>
     </footer>
   );
 };
