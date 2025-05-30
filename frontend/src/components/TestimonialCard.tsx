1f02dc4522cb98f88dfc29d91204871609afd17c
Daniel Leith <elixerprince@gmail.com>
2025-05-29 21:25:02 -0500

    Add TestimonialCard component and integrate it into TestimonialSection; enhance layout and structure

---
+++ frontend/src/components/TestimonialCard.tsx
@@ -0,0 +1,17 @@
+const TestimonialCard = () => {
+  return (
+    <article className="flex gap-4 border-4">
+      <img
+        className="border-4 border-amber-400"
+        src=""
+        alt="Testimonial Image"
+      />
+      <div>
+        <h3 className="text-2xl font-bold">Testimonial Name</h3>
+        <p>Testimonial Description</p>
+      </div>
+    </article>
+  );
+};
+
+export default TestimonialCard;
--- frontend/src/components/TestimonialSection.tsx
+++ frontend/src/components/TestimonialSection.tsx
@@ -1,13 +1,26 @@
+import TestimonialCard from "./TestimonialCard";
+
 const TestimonialSection = () => {
   return (
     <section
       id="testimonials"
       aria-labelledby="testimonials-header"
-      className="border-2 border-amber-400"
+      className="scroll-mt-24 px-4 py-8"
     >
       <header>
-        <h2 id="testimonials-header">WHAT PEOPLE SAY</h2>
+        <h2
+          id="testimonials-header"
+          className="pb-4 text-center text-4xl font-black"
+        >
+          WHAT PEOPLE SAY
+        </h2>
       </header>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
+      </div>
     </section>
   );
 };
