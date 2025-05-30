9cd2f58ecdb12edd6a69d0af1f54136166927d18
Daniel Leith <elixerprince@gmail.com>
2025-05-29 21:57:50 -0500

    Enhance ContactSection layout; adjust padding, margin, and structure for improved usability

--- frontend/src/components/ContactSection.tsx
+++ frontend/src/components/ContactSection.tsx
@@ -1,11 +1,12 @@
 import WhatsappQrCode from "../assets/whatsapp-qr-code.png";
+import { FaPaperPlane } from "react-icons/fa6";
 
 const ContactSection = () => {
   return (
     <section
       id="contact"
       aria-labelledby="contact-header"
-      className="px-4 py-8"
+      className="px-4 py-10 sm:px-16"
     >
       <header>
         <h2
@@ -15,29 +16,35 @@ const ContactSection = () => {
           CONTACT US
         </h2>
       </header>
-      <div className="flex flex-col gap-4 lg:flex-row">
-        <form className="mb-8 flex flex-1 flex-col gap-4">
+      <div className="flex flex-col justify-center gap-4 md:flex-row">
+        <form className="mb-8 flex h-full w-full flex-1 flex-col items-center justify-center gap-4 self-center px-4">
           <input
-            className="rounded-md border-3 px-4 py-2"
+            className="w-full rounded-md border-3 px-4 py-2"
             type="text"
             placeholder="John Doe"
           />
           <input
-            className="rounded-md border-3 px-4 py-2"
+            className="w-full rounded-md border-3 px-4 py-2"
             type="email"
             placeholder="johndoe@example.com"
           />
           <textarea
             id="message"
-            className="rounded-md border-3 px-4 py-2"
+            className="w-full rounded-md border-3 px-4 py-2"
             name="message"
             placeholder="Write your message here..."
             rows={4}
           ></textarea>
+          <button
+            type="submit"
+            className="from-brand-green to-brand-blue mt-4 flex items-center gap-2 rounded-lg bg-linear-to-r px-8 py-4 text-xl font-black text-white md:self-start"
+          >
+            Send Message <FaPaperPlane className="text-2xl" />
+          </button>
         </form>
-        <div className="flex flex-1 items-center justify-center">
+        <div className="mt-8 flex flex-1 items-center justify-center">
           <img
-            className="border-brand-green max-w-full rounded-md border-8"
+            className="border-brand-green -mt-8 w-80 max-w-full rounded-md border-6"
             src={WhatsappQrCode}
             alt="Whatsapp QR Code"
           />
