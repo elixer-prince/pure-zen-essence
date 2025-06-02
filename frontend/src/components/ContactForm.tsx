ad2c862bcc45aa172aeb16e22e80c9f14d367f05
Daniel Leith <elixerprince@gmail.com>
2025-06-02 01:15:35 -0500

    Refactor ContactForm layout: add labels for inputs, adjust spacing, and improve structure for better readability

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -3,35 +3,61 @@ import { FaPaperPlane } from "react-icons/fa6";
 const ContactForm = () => {
   return (
     <form
-      className="mb-8 flex h-full w-full flex-1 flex-col items-center justify-center gap-4 self-center px-4 sm:px-8"
+      className="mb-8 flex h-full w-full flex-1 flex-col items-center justify-center gap-6 self-center px-4 sm:px-8"
       action="https://formspree.io/f/xeokjzab"
       method="POST"
     >
-      <input
-        className="w-full rounded-md border-3 px-4 py-2 text-base sm:text-lg"
-        type="text"
-        name="name"
-        placeholder="John Doe"
-        required
-      />
-      <input
-        className="w-full rounded-md border-3 px-4 py-2 text-base sm:text-lg"
-        type="email"
-        name="email"
-        placeholder="johndoe@example.com"
-        required
-      />
-      <textarea
-        id="message"
-        className="w-full resize-none rounded-md border-3 px-4 py-2 text-base sm:text-lg"
-        name="message"
-        placeholder="Write your message here..."
-        rows={4}
-        required
-      ></textarea>
+      <div className="w-full">
+        <label
+          className="mb-2 inline-block text-lg font-bold select-none"
+          htmlFor="name"
+        >
+          Full Name
+        </label>
+        <input
+          id="name"
+          className="w-full rounded-md border-3 px-4 py-2 text-base sm:text-lg"
+          type="text"
+          name="name"
+          placeholder="John Doe"
+          required
+        />
+      </div>
+      <div className="w-full">
+        <label
+          className="mb-2 inline-block text-lg font-bold select-none"
+          htmlFor="email"
+        >
+          Email Address
+        </label>
+        <input
+          id="email"
+          className="w-full rounded-md border-3 px-4 py-2 text-base sm:text-lg"
+          type="email"
+          name="email"
+          placeholder="johndoe@example.com"
+          required
+        />
+      </div>
+      <div className="w-full">
+        <label
+          className="mb-2 inline-block text-lg font-bold"
+          htmlFor="message"
+        >
+          Message
+        </label>
+        <textarea
+          id="message"
+          className="w-full resize-none rounded-md border-3 px-4 py-2 text-base sm:text-lg"
+          name="message"
+          placeholder="Write your message here..."
+          rows={4}
+          required
+        ></textarea>
+      </div>
       <button
         type="submit"
-        className="from-brand-green to-brand-blue mt-4 flex cursor-pointer items-center gap-2 rounded-lg bg-linear-to-r px-6 py-3 text-lg font-black text-white transition-all duration-300 select-none hover:scale-105 active:scale-95 sm:px-8 sm:py-4 sm:text-xl md:self-start"
+        className="from-brand-green to-brand-blue mt-4 flex cursor-pointer items-center gap-2 rounded-lg bg-linear-to-r px-6 py-3 text-lg font-black text-white transition-all duration-300 select-none hover:scale-105 active:scale-95 sm:px-8 sm:py-4 sm:text-xl"
       >
         Send Email <FaPaperPlane className="text-xl sm:text-2xl" />
       </button>
