6c62d47fde1343d132a39dc06db55d51837cbcc7
Daniel Leith <elixerprince@gmail.com>
2025-06-01 22:20:44 -0500

    Add FaqQuestion component and update FaqSection layout

---
+++ frontend/src/components/FaqQuestion.tsx
@@ -0,0 +1,10 @@
+const FaqQuestion = () => {
+  return (
+    <details className="">
+      <summary className="cursor-pointer border">Question 1</summary>
+      <p>Answer 1</p>
+    </details>
+  );
+};
+
+export default FaqQuestion;
--- frontend/src/components/FaqSection.tsx
+++ frontend/src/components/FaqSection.tsx
@@ -1,13 +1,26 @@
+import FaqQuestion from "./FaqQuestion";
+
 const FaqSection = () => {
   return (
     <section
       id="faqs"
+      className="scroll-mt-24 gap-4 bg-neutral-100 px-4 py-8 pb-16"
       aria-labelledby="faqs-header"
-      className="border-2 border-amber-400"
     >
       <header>
-        <h2 id="faqs-header">FAQs</h2>
+        <h2
+          id="faqs-header"
+          className="pb-8 text-center text-4xl font-black sm:text-5xl md:text-6xl"
+        >
+          FAQs
+        </h2>
       </header>
+      <div className="grid gap-4 lg:grid-cols-2">
+        <FaqQuestion />
+        <FaqQuestion />
+        <FaqQuestion />
+        <FaqQuestion />
+      </div>
     </section>
   );
 };
