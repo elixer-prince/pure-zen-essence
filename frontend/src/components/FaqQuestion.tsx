4b0214de5d1b307703306faeedaf73f5a9fac6ef
Daniel Leith <elixerprince@gmail.com>
2025-06-02 01:58:36 -0500

    Update FaqQuestion and FaqSection components: pass question prop to FaqQuestion and enhance styling for better presentation

--- frontend/src/components/FaqQuestion.tsx
+++ frontend/src/components/FaqQuestion.tsx
@@ -1,7 +1,9 @@
-const FaqQuestion = () => {
+const FaqQuestion = ({ question }: { question: string }) => {
   return (
-    <details className="">
-      <summary className="cursor-pointer border">Question 1</summary>
+    <details className="rounded-full bg-white">
+      <summary className="cursor-pointer rounded-full border border-none bg-linear-to-r from-black to-black/95 px-8 py-4 text-xl font-bold text-white">
+        {question}
+      </summary>
       <p>Answer 1</p>
     </details>
   );
--- frontend/src/components/FaqSection.tsx
+++ frontend/src/components/FaqSection.tsx
@@ -4,22 +4,30 @@ const FaqSection = () => {
   return (
     <section
       id="faqs"
-      className="scroll-mt-24 gap-4 bg-neutral-100 px-4 py-8 pb-16"
+      className="bg-brand-blue scroll-mt-24 gap-4 px-24 py-16 pb-16"
       aria-labelledby="faqs-header"
     >
       <header>
         <h2
           id="faqs-header"
-          className="pb-8 text-center text-4xl font-black sm:text-5xl md:text-6xl"
+          className="pb-8 text-center text-4xl font-black text-white sm:text-5xl md:text-6xl"
         >
           FAQs
         </h2>
       </header>
       <div className="grid gap-4 lg:grid-cols-2">
-        <FaqQuestion />
-        <FaqQuestion />
-        <FaqQuestion />
-        <FaqQuestion />
+        <div>
+          <FaqQuestion question="What is Pure Zen Essence?" />
+        </div>
+        <div>
+          <FaqQuestion question="What is Pure Zen Essence?" />
+        </div>
+        <div>
+          <FaqQuestion question="What is Pure Zen Essence?" />
+        </div>
+        <div>
+          <FaqQuestion question="What is Pure Zen Essence?" />
+        </div>
       </div>
     </section>
   );
