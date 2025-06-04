e9888ced87bf762d89d978b75face314623904d5
Daniel Leith <elixerprince@gmail.com>
2025-06-04 12:34:38 -0500

    Modify navbar comment for clarity

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = () => {
   };
 
   return (
-    // NAVBAR
+    // HOMEPAGE NAVBAR
     <nav
       className="to-brand-darkblue fixed top-0 right-0 -bottom-12 left-0 z-10 h-24 bg-gradient-to-r from-black p-4 text-white shadow shadow-black backdrop-blur-sm transition-all duration-500 select-none"
       ref={navbarRef}
