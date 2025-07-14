bc2c1e008fd29ddb3696d48298d15e28aa7e599f
Daniel Leith <elixerprince@gmail.com>
2025-07-14 14:44:36 -0500

    chore: add the font awesome cdn and website favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="en-JM">
   <head>
     <meta charset="UTF-8" />
@@ -7,10 +8,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>{{ $pageTitle }}</title>
+    {{-- TODO: Switch to an optimized favicon --}}
+    <link rel="icon" href="{{ asset('images/brand-icon.png') }}" type="image/x-icon" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+      integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
     @vite('resources/css/app.css')
   </head>
 
   <body>
+    <x-navbar />
+
     <main>
       {{ $slot }}
     </main>
