b4d6a34aca744c037f10a7000fc29a5b68123497
Daniel Leith <elixerprince@gmail.com>
2025-07-13 22:40:33 -0500

    refactor: create a reusable layout component for modularity

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en-JM">
+<x-layout>
+  {{-- TODO: Add a description --}}
+  <x-slot name="pageTitle">Pure Zen Essence</x-slot>
+  <x-slot name="pageDescription">...</x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="author" cotent="Daniel Leith">
-    <meta name="description" cotent="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pure Zen Essence</title>
-</head>
-
-<body>
-    <h1>Hello Pure Zen Essence</h1>
-</body>
-
-</html>
+</x-layout>
