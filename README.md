0b3ec0f47ea9ced6ce19d3ed69f55668852bcdf8
Daniel Leith <elixerprince@gmail.com>
2025-07-13 20:13:53 -0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">🌿 Pure Zen Essence 🌿</h1>
+
+Content should go here...
+
+<p align="center">Copyright &copy; Elixer Enterprise</p>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-JM">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" cotent="Daniel Leith">
+    <meta name="description" cotent="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pure Zen Essence</title>
+</head>
+
+<body>
+    <h1>Hello Pure Zen Essence</h1>
+</body>
+
+</html>
