d733c4a407339efc0cfcc03e5bf8d620f430a6af
Daniel Leith <elixerprince@gmail.com>
2025-07-15 15:01:30 -0500

    style(header): adjust the header styling to appear more structured and make it easier to read

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,10 @@
-<header
-  class="mt-[var(--navbar-height)] flex min-h-[calc(100dvh-var(--navbar-height))] items-start justify-center px-4 pt-6"
->
+<header class="mt-[var(--navbar-height)] flex items-start justify-center px-8 pt-16 pb-38 md:pt-38">
   <x-navbar />
 
   <div class="flex flex-col items-center gap-8 text-center">
     <div class="flex flex-col items-center gap-8">
       <h1 class="text-4xl font-bold">
-        Do you want to be EFFORTLESSLY
+        Do you want to be effortlessly
         <span class="bg-brand-green-500 bg-clip-text font-black text-transparent">BEAUTIFUL?</span>
       </h1>
       <p class="text-2xl font-semibold">
@@ -16,7 +14,7 @@
     </div>
 
     <div class="flex items-center gap-4">
-      <x-button class="bg-black hover:bg-black/95" href="#about">Learn More</x-button>
+      <x-button class="bg-black hover:bg-neutral-900" href="#about">Learn More</x-button>
       <x-button class="bg-brand-green-500 hover:bg-brand-green-600" href="#contact">Contact Us</x-button>
     </div>
   </div>
