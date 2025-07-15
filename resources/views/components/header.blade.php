fee837eb5adc2e12540996be76505db4f64da4d0
Daniel Leith <elixerprince@gmail.com>
2025-07-14 19:36:41 -0500

    refactor(header): add basic text content to the header and make it responsive

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,17 @@
-<header class="mt-[var(--navbar-height)] h-screen items-center border border-amber-500 p-4">
+<header
+  class="mt-[var(--navbar-height)] flex min-h-[calc(100dvh-var(--navbar-height))] items-center justify-center px-6 py-8 sm:px-12"
+>
   <x-navbar />
 
-  <div class="flex flex-col items-center gap-8 border border-red-500 text-center">
+  <div class="flex flex-col items-center gap-8 text-center">
     <div class="flex flex-col items-center gap-8">
-      <h1 class="border text-5xl font-bold">Do you want effortless beauty?</h1>
-      <p class="text-2xl">
+      <h1 class="text-5xl font-bold">
+        Do you want to be effortlessly
+        <span class="bg-brand-green-500 bg-clip-text font-black text-transparent">BEAUTIFUL?</span>
+      </h1>
+      <p class="text-3xl font-semibold">
         Find your natural essence with
-        <span class="hover:text-brand-green-500 transition-colors duration-500">Pure Zen Essence!</span>
+        <span class="hover:text-brand-green-500 font-black transition-colors duration-500">Pure Zen Essence!</span>
       </p>
     </div>
 
