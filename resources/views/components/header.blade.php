c5563f1c6f64019b1ed9bd399f073b1abab82b9e
Daniel Leith <elixerprince@gmail.com>
2025-07-15 00:46:51 -0500

    style(header): change the text so that it pops more visually

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,15 @@
 <header
-  class="mt-[var(--navbar-height)] flex min-h-[calc(100dvh-var(--navbar-height))] items-center justify-center px-6 py-8 sm:px-12"
+  class="mt-[var(--navbar-height)] flex min-h-[calc(100dvh-var(--navbar-height))] items-start justify-center px-4 pt-6"
 >
   <x-navbar />
 
   <div class="flex flex-col items-center gap-8 text-center">
     <div class="flex flex-col items-center gap-8">
-      <h1 class="text-5xl font-bold">
-        Do you want to be effortlessly
+      <h1 class="text-4xl font-bold">
+        Do you want to be EFFORTLESSLY
         <span class="bg-brand-green-500 bg-clip-text font-black text-transparent">BEAUTIFUL?</span>
       </h1>
-      <p class="text-3xl font-semibold">
+      <p class="text-2xl font-semibold">
         Find your natural essence with
         <span class="hover:text-brand-green-500 font-black transition-colors duration-500">Pure Zen Essence!</span>
       </p>
