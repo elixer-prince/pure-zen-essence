6c5d83528f72e741654cf4b75e53d60ddd8103c8
Daniel Leith <elixerprince@gmail.com>
2025-07-16 16:14:10 -0500

    feat(auth): create the boilerplate for the auth endpoints

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.layout>
+  <x-slot name="pageTitle">Pure Zen Essence | Login</x-slot>
+  <x-slot name="pageDescription">...</x-slot>
+
+  <main>Login on this page...</main>
+</x-layouts.layout>
---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.layout>
+  <x-slot name="pageTitle">Pure Zen Essence | Sign Up</x-slot>
+  <x-slot name="pageDescription">...</x-slot>
+
+  <main>Sign up on this page...</main>
+</x-layouts.layout>
