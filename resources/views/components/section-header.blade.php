b521e0ccfd328175df4372b80ec1b361d133f1d6
Daniel Leith <elixerprince@gmail.com>
2025-07-16 11:46:11 -0500

    refactor: extract a section header component for modularity

---
+++ resources/views/components/section-header.blade.php
@@ -0,0 +1,9 @@
+@php
+  $classes = 'border-brand-blue-500 mb-8 w-fit rounded-lg border-b-8 pb-2 text-center text-4xl font-bold';
+@endphp
+
+<header>
+  <h2 {{ $attributes(['class' => $classes, 'id' => $id]) }}>
+    {{ $slot }}
+  </h2>
+</header>
