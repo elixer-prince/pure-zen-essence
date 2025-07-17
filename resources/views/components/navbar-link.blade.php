ba194df1e54fb8b26407610480b670d56793f3b5
Daniel Leith <elixerprince@gmail.com>
2025-07-16 20:55:49 -0500

    style(navbar): add a subtle style change to the links when the are clicked

--- resources/views/components/navbar-link.blade.php
+++ resources/views/components/navbar-link.blade.php
@@ -1,6 +1,6 @@
 @php
   $classes =
-    'hover:text-brand-green-500 inline-block p-2 transition-all duration-300 first:pl-4 last:pr-4 hover:bg-white/15 hover:px-4';
+    'hover:text-brand-green-500 inline-block p-2 transition-all duration-300 first:pl-4 last:pr-4 hover:bg-white/15 hover:px-4 active:bg-white/25';
 @endphp
 
 <a {{ $attributes(['class' => $classes]) }}>
