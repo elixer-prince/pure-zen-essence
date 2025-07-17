45a21018420ce41656bf98b8c00945375fab253f
Daniel Leith <elixerprince@gmail.com>
2025-07-17 11:20:35 -0500

    style(navbar): make the navbar padding even on mobile screens

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav
-  class="fixed top-0 right-0 left-0 z-10 flex h-[var(--navbar-height)] items-center justify-between bg-neutral-900 px-8 py-4 font-bold text-white"
+  class="fixed top-0 right-0 left-0 z-10 flex h-[var(--navbar-height)] items-center justify-between bg-neutral-900 p-4 font-bold text-white sm:px-8"
 >
   <div class="flex items-center gap-8">
     <a class="flex items-center gap-2" href="/">
@@ -9,7 +9,9 @@
       </span>
     </a>
 
-    <ul class="hidden items-center overflow-hidden rounded-md border-2 lg:flex [&>li:first-child>a]:pl-4 [&>li:last-child>a]:pr-4">
+    <ul
+      class="hidden items-center overflow-hidden rounded-md border-2 lg:flex [&>li:first-child>a]:pl-4 [&>li:last-child>a]:pr-4"
+    >
       <li><x-navbar-link href="#">HOME</x-navbar-link></li>
       <li><x-navbar-link href="#about">ABOUT US</x-navbar-link></li>
       <li><x-navbar-link href="#products">OUR PRODUCTS</x-navbar-link></li>
