acab120f04b0029c64257381fa350edd2f5d80a1
Daniel Leith <elixerprince@gmail.com>
2025-07-17 17:01:00 -0500

    style(header): adjust the spacing of the cta text on mobile screens

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,15 +1,16 @@
-<header class="mt-[var(--navbar-height)] flex items-start justify-center gap-8 px-8 pt-16 pb-38 md:pt-38">
+<header class="mt-[var(--navbar-height)] flex items-start justify-center gap-8 px-4 pt-14 pb-38 sm:px-8 md:pt-38">
   <x-navbar />
 
   <div class="flex-1.5 flex flex-col items-center gap-12 text-center md:px-16">
     <div class="flex flex-col items-center gap-8">
-      <h1 class="text-4xl font-bold">
-        Do you want to be effortlessly
-        <span class="bg-brand-green-500 bg-clip-text font-black text-transparent">BEAUTIFUL?</span>
+      <h1 class="text-4xl font-bold uppercase">
+        Have you ever wanted to be effortlessly
+        <span class="bg-brand-green-500 bg-clip-text font-black text-transparent">beautiful</span>
       </h1>
       <p class="text-2xl">
         Find your natural essence with
-        <span class="hover:text-brand-green-500 font-black transition-colors duration-500">Pure Zen Essence!</span>
+        <span class="hover:text-brand-green-500 font-black transition-colors duration-500">Pure Zen Essence</span>
+        <span class="font-black">!</span>
       </p>
     </div>
 
