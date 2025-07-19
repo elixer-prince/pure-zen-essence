b8e211240e53ab01dbbb3c05c8c6a64b4318c1b7
Daniel Leith <elixerprince@gmail.com>
2025-07-19 13:06:49 -0500

    style: add the section heading marggin to the header element ratther than the h2 element

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,8 +1,8 @@
 @php
-  $classes = 'mb-8 w-fit rounded-lg border-b-8 border-neutral-500 pb-2 text-center text-4xl font-bold';
+  $classes = 'w-fit rounded-lg border-b-8 border-neutral-500 pb-2 text-center text-4xl font-bold';
 @endphp
 
-<header>
+<header class="mb-8">
   <h2 {{ $attributes(['class' => $classes, 'id' => $id]) }}>
     {{ $slot }}
   </h2>
