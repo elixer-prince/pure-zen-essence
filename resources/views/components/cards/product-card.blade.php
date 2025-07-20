dccc7c5c468fe1476b23539e411f6ad891915565
Daniel Leith <elixerprince@gmail.com>
2025-07-20 00:21:29 -0500

    style(product-card): adjust the styling of the product card

--- resources/views/components/cards/product-card.blade.php
+++ resources/views/components/cards/product-card.blade.php
@@ -1,4 +1,4 @@
-<article class="h-90 w-90 max-w-full perspective-midrange">
+<article class="h-90 w-full perspective-midrange md:w-90">
   <div class="group h-full transition-transform duration-500 transform-3d">
     <div
       class="border-brand-darkblue-950/50 absolute h-full w-full overflow-hidden rounded-tl-4xl rounded-br-4xl border-2 bg-white backface-hidden"
@@ -15,7 +15,7 @@
           <h3 class="mb-3 text-2xl font-bold">{{ $title }}</h3>
           <p class="mb-5 text-black/75">{{ $description }}</p>
         </div>
-        <x-button class="bg-neutral-950 text-neutral-50 text-sm hover:bg-neutral-800" type="button">More Info</x-button>
+        <x-button class="bg-neutral-950 text-sm text-neutral-50 hover:bg-neutral-800" type="button">More Info</x-button>
       </div>
     </div>
 
