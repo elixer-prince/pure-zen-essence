8ea741919f9f767ba7f25d02215898306a917458
Daniel Leith <elixerprince@gmail.com>
2025-07-21 03:37:08 -0500

    feat: add temporary component for livewire testing

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 
 <html lang="en-JM" class="not-motion-reduce:scroll-smooth">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="author" cotent="Daniel Leith" />
     <meta name="description" cotent="{{ $pageDescription }}" />
@@ -10,23 +11,18 @@
     <title>{{ $pageTitle }}</title>
     {{-- TODO: Switch to an optimized favicon --}}
     <link rel="icon" href="{{ asset('images/brand-icon.png') }}" type="image/x-icon" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-      integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet" />
     @vite('resources/css/app.css')
-  </head>
+</head>
 
-  <body class="font-roboto h-screen text-neutral-950 select-none">
+<body class="font-roboto h-screen text-neutral-950 select-none">
     {{ $slot }}
-  </body>
+</body>
+
 </html>
