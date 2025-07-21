a8b9e57998b74307581267274acdaece63f51c0d
Daniel Leith <elixerprince@gmail.com>
2025-07-21 18:00:05 -0500

    feat: add a page description

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,12 @@
 <x-layouts.normal>
-    <x-slot name="pageTitle">Pure Zen Essence | Beauty By Nature</x-slot>
-    <x-slot name="pageDescription">...</x-slot>
+    <x-slot name="pageTitle">
+        Pure Zen Essence | Beauty By Nature
+    </x-slot>
+    <x-slot name="pageDescription">
+        Welcome to Pure Zen Essence, a family-owned natural products business proudly rooted in the heart of Wakefield
+        Savannah, St. Catherine, Jamaica. Founded by the Carty family, our mission is to provide 100% Natural products
+        that promote overall health and wellness.
+    </x-slot>
 
     <x-section id="about" class="relative flex-row gap-16 bg-neutral-950 pt-34 lg:pt-20"
         aria-labelledby="about-header">
