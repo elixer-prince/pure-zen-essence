f430253f6c8a3eb8891dd84518655b4f51b87aec
Daniel Leith <elixerprince@gmail.com>
2025-07-21 03:37:41 -0500

    feat(footer): add the social media icons

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,11 @@
-<footer class="bg-brand-blue-950 p-16 font-bold text-neutral-50">
-  <p class="text-center">Copyright &copy; Elixer Enterprise | {{ date('Y') }}</p>
+<footer class="bg-brand-blue-950 flex flex-col gap-8 px-8 py-16 text-neutral-50">
+  <ul class="flex justify-center gap-8">
+    <li>
+      <a href=""><i class="fa-brands fa-instagram text-4xl"></i></a>
+    </li>
+    <li>
+      <a href=""><i class="fa-brands fa-tiktok text-4xl"></i></a>
+    </li>
+  </ul>
+  <p class="text-center text-sm font-bold">Copyright &copy; Elixer Enterprise | {{ date('Y') }}</p>
 </footer>
