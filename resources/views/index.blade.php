64c17cc735c7bf4b3835c39413adcaa59ba11d54
Daniel Leith <elixerprince@gmail.com>
2025-07-21 03:50:39 -0500

    style(index): increase the tab spacing to 4 for increased readability

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,34 @@
 <x-layouts.normal>
-  <x-slot name="pageTitle">Pure Zen Essence | Beauty By Nature</x-slot>
-  {{-- TODO: Add a description --}}
-  <x-slot name="pageDescription">...</x-slot>
+    <x-slot name="pageTitle">Pure Zen Essence | Beauty By Nature</x-slot>
+    <x-slot name="pageDescription">...</x-slot>
 
-  <x-section
-    id="about"
-    class="relative flex-row gap-16 bg-black pt-34 text-white lg:pt-20"
-    aria-labelledby="about-header"
-  >
-    <x-about-image />
+    <x-section id="about" class="relative flex-row gap-16 bg-neutral-950 pt-34 lg:pt-20"
+        aria-labelledby="about-header">
+        <x-about-image />
 
-    <div class="flex flex-2 flex-col items-center">
-      <x-section-heading id="about-header">About Us</x-section-heading>
+        <div class="flex flex-2 flex-col items-center">
+            <x-section-heading id="about-header" class="text-neutral-50 border-b-brand-blue-500">About
+                Us</x-section-heading>
 
-      <div class="flex flex-col items-center gap-8">
-        <x-about-card heading="Our Vision">
-          Our vision is to be a trusted family business that nurtures enduring relationships, champions sustainable
-          farming practices, and makes a meaningful impact on both our community and the environment.
-        </x-about-card>
-        <x-about-card heading="Our Mission">
-          As a family-owned business, we are committed to cultivating high-quality products with care, integrity, and
-          profound respect for the land. Through our hard work and shared values, we strive to nourish our community
-          while safeguarding our legacy for future generations.
-        </x-about-card>
-      </div>
-    </div>
-  </x-section>
+            <div class="flex flex-col items-center gap-8">
+                <x-cards.about-card heading="Our Vision">
+                    Our vision is to be a trusted family business that nurtures enduring relationships, champions
+                    sustainable
+                    farming practices, and makes a meaningful impact on both our community and the environment.
+                </x-cards.about-card>
+                <x-cards.about-card heading="Our Mission">
+                    As a family-owned business, we are committed to cultivating high-quality products with care,
+                    integrity, and
+                    profound respect for the land. Through our hard work and shared values, we strive to nourish our
+                    community
+                    while safeguarding our legacy for future generations.
+                </x-cards.about-card>
+            </div>
+        </div>
+    </x-section>
 
-  <x-section id="products" aria-labelledby="products-header">
-    <x-section-heading id="products-header" class="border-b-neutral-500">Our Products</x-section-heading>
+    <x-section id="products" class="bg-neutral-100" aria-labelledby="products-header">
+        <x-section-heading id="products-header" class="border-b-brand-green-500">Our Products</x-section-heading>
 
         <x-grid class="sm:grid-cols-2 justify-center lg:grid-cols-3">
             @foreach ($products as $product)
@@ -39,11 +39,11 @@
                         </x-slot>
                 </x-cards.product-card>
             @endforeach
-    </x-grid>
-  </x-section>
+        </x-grid>
+    </x-section>
 
   <x-section id="testimonials" aria-labelledby="testimonials-header">
-    <x-section-heading id="testimonials-header">Testimonials</x-section-heading>
+        <x-section-heading id="testimonials-header">Testimonials</x-section-heading>
 
         <x-grid class="sm:grid-cols-2 lg:grid-cols-3">
             <x-cards.testimonial-card></x-cards.testimonial-card>
@@ -57,7 +57,7 @@
         <x-section-heading id="faqs-header"
             class="border-b-brand-green-500 text-brand-darkblue-50">FAQs</x-section-heading>
 
-    <x-grid class="md:grid-cols-2">
+        <x-grid class="md:grid-cols-2">
             <div>
                 <x-cards.faq-card>
                     <x-slot name="question">Is the world flat?</x-slot>
@@ -76,8 +76,8 @@
                     <x-slot name="answer">Yes it is</x-slot>
                 </x-cards.faq-card>
             </div>
-    </x-grid>
-  </x-section>
+        </x-grid>
+    </x-section>
 
     <x-section id="contact" aria-labelledby="contact-header">
         <x-forms.form class="w-160 max-w-full p-12 border-3 border-neutral-400 rounded-lg md:p-20">
@@ -106,5 +106,5 @@
                 </span>
             </x-button>
         </x-forms.form>
-  </x-section>
+    </x-section>
 </x-layouts.normal>
