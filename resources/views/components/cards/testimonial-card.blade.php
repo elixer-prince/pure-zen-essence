e8ac75daf51509146f4dc392daaf6cfee8f404f8
Daniel Leith <elixerprince@gmail.com>
2025-07-21 03:38:33 -0500

    style: improve the tab spacing to 4 from 2 for readability

--- resources/views/components/cards/testimonial-card.blade.php
+++ resources/views/components/cards/testimonial-card.blade.php
@@ -1,19 +1,15 @@
 <div class="relative mt-12 flex rounded-lg border-2 border-neutral-500 bg-neutral-50 p-8 pt-18">
-  <div
-    class="absolute top-0 left-1/2 aspect-square h-28 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-3 border-neutral-50 bg-neutral-500"
-  >
-    <img
-      class="h-full w-full"
-      src="{{ asset('images/placeholder-image.svg') }}"
-      alt="Testimonial Image"
-    />
-  </div>
-  <div>
-    <h3 class="mb-4 text-2xl font-bold">Testimonial Title</h3>
-    <p class="text-black/75">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam beatae dolore explicabo iusto excepturi
-      veritatis dolorum, aspernatur consectetur debitis nihil fugit aut molestiae tenetur ducimus soluta repellat vero
-      qui nemo.
-    </p>
-  </div>
+    <div
+        class="absolute top-0 left-1/2 aspect-square h-28 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-3 border-neutral-50 bg-neutral-500">
+        <img class="h-full w-full" src="{{ asset('images/placeholder-image.svg') }}" alt="Testimonial Image" />
+    </div>
+    <div>
+        <h3 class="mb-4 text-2xl font-bold">Testimonial Title</h3>
+        <p class="text-black/75">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam beatae dolore explicabo iusto excepturi
+            veritatis dolorum, aspernatur consectetur debitis nihil fugit aut molestiae tenetur ducimus soluta repellat
+            vero
+            qui nemo.
+        </p>
+    </div>
 </div>
