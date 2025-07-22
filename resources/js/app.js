fe6d3e0eb7d1d74344cc596989c2815e746165a7
Daniel Leith <elixerprince@gmail.com>
2025-07-21 19:07:05 -0500

    feat(header): add a gsap stagger animation to the hero cta content

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,10 @@
 import "./bootstrap";
 
 import gsap from "gsap";
+
+gsap.from(".cta-content", {
+    y: 300,
+    duration: 1.25,
+    stagger: 0.25,
+    opacity: 0,
+});
--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,14 +1,14 @@
 <header
-    class="mt-[var(--navbar-height)] flex justify-center gap-16 px-8 pt-14 pb-38 sm:pt-24 md:px-16 lg:py-24 xl:py-32">
+    class="mt-[var(--navbar-height)] lg:items-center flex justify-center gap-16 px-8 pt-14 pb-38 sm:pt-24 md:px-16 lg:py-24 xl:py-32">
     <x-navbar />
 
     <div class="flex flex-[1.5] flex-col items-center gap-12 text-center">
         <div class="flex flex-col items-center gap-8">
-            <h1 class="text-4xl font-bold uppercase xl:text-5xl">
+            <h1 class="text-4xl cta-content font-bold uppercase xl:text-5xl">
                 Have you ever wanted to be effortlessly
                 <span class="bg-brand-green-500 bg-clip-text font-black text-transparent">beautiful</span>
             </h1>
-            <p class="text-2xl">
+            <p class="text-2xl cta-content">
                 Find your natural essence with
                 <span class="hover:text-brand-green-500 font-black transition-colors duration-500">Pure Zen
                     Essence</span>
@@ -16,7 +16,7 @@
             </p>
         </div>
 
-        <div class="flex items-center gap-4">
+        <div class="flex cta-content items-center gap-4">
             <x-button class="border-2 hover:bg-neutral-800 bg-neutral-950 text-neutral-50" href="#about">
                 Learn More
                 <span class="hidden sm:inline">
@@ -32,5 +32,7 @@
         </div>
     </div>
 
-    <div class="hidden h-full flex-1 lg:block">Image of a beatiful woman...</div>
+    <div class="hidden overflow-hidden mr-20 max-w-90 lg:block rounded-lg h-full flex-1">
+        <img class="w-full h-full" src="https://picsum.photos/300/300" alt="A beautiful woman">
+    </div>
 </header>
