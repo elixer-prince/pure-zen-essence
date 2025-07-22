7c50df81e34e85a08e06aebda1ed96e6f3199c81
Daniel Leith <elixerprince@gmail.com>
2025-07-22 00:09:01 -0500

    style(product-card): change the border colour and image bg colour of the card

--- resources/views/components/cards/product-card.blade.php
+++ resources/views/components/cards/product-card.blade.php
@@ -2,10 +2,10 @@
     class="group/card hover:-translate-y-1 transition-transform duration-500 perspective-midrange md:w-90">
     <div x-bind:class="flipped ? '-rotate-y-180' : ''" class="transition-transform duration-500 relative transform-3d">
         <div
-            class="border-brand-darkblue-950/50 h-full flex flex-col overflow-hidden rounded-tl-4xl rounded-br-4xl border-2 bg-white backface-hidden">
+            class="border-brand-green-500 h-full flex flex-col overflow-hidden rounded-tl-4xl rounded-br-4xl border-2 bg-white backface-hidden">
             <div class="h-52 overflow-hidden">
-                <img class=" w-full bg-neutral-700 object-cover transition-transform duration-500 group-hover/card:scale-115"
-                    src="{{ asset('images/placeholder-image.svg') }}" alt="Product Image" />
+                <img class="h-full w-full object-cover transition-transform duration-500 group-hover/card:scale-115"
+                    src="https://picsum.photos/seed/{{ rand(1, 100000) }}/350/350" alt="Product Image" />
             </div>
             <div class="p-8">
                 <div>
@@ -34,10 +34,15 @@
                 <div class="flex gap-2 flex-wrap">
                     @foreach ($product->sizes as $size)
                         <div
-                            class="cursor-pointer not-first:border-2 not-first:border-brand-darkblue-950 transition-colors duration-500 first:bg-brand-darkblue-950 px-4 not-first:text-brand-darkblue-950 font-bold not-first:hover:bg-brand-darkblue-950/25 first:text-brand-darkblue-50 text-sm py-1 rounded-full">
+                            class="cursor-pointer text-xs not-first:border-2 not-first:border-brand-darkblue-950 transition-colors duration-500 first:bg-brand-darkblue-950 px-4 not-first:text-brand-darkblue-950 font-bold not-first:hover:bg-brand-darkblue-950/25 first:text-brand-darkblue-50 md:text-sm py-1 rounded-full">
                             {{ $size['name'] }}
                         </div>
                     @endforeach
+                </div>
+                <div>
+                    <p class="py-4">
+                        {{ $product->description }}
+                    </p>
                 </div>
             </div>
         </div>
