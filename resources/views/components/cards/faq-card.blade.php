7d39dc864197ab544aca50a130c82d1511865432
Daniel Leith <elixerprince@gmail.com>
2025-07-21 23:08:20 -0500

    style: fummary the summary open styling of the faq card

--- resources/views/components/cards/faq-card.blade.php
+++ resources/views/components/cards/faq-card.blade.php
@@ -1,5 +1,6 @@
-<details class="rounded-lg bg-white">
-    <summary class="bg-brand-blue-500 cursor-pointer rounded-lg px-8 py-4 font-bold text-white open:rounded-b-md">
+<details class="rounded-lg group/faq bg-white">
+    <summary
+        class="bg-brand-blue-500 group-open/faq:rounded-b-none cursor-pointer rounded-lg px-8 py-4 font-bold text-white">
         {{ $question }}
     </summary>
     <p class="px-8 py-4">
