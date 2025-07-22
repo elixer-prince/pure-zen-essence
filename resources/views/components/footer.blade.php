6910212b1778084214201c9b17166866aa9b7478
Daniel Leith <elixerprince@gmail.com>
2025-07-22 00:09:59 -0500

    style(footer): add a hover style to the social links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,17 @@
 <footer class="bg-brand-blue-950 flex flex-col gap-8 px-8 py-16 text-neutral-50">
-  <ul class="flex justify-center gap-8">
-    <li>
-      <a href=""><i class="fa-brands fa-instagram text-4xl"></i></a>
-    </li>
-    <li>
-      <a href=""><i class="fa-brands fa-tiktok text-4xl"></i></a>
-    </li>
-  </ul>
-  <p class="text-center text-sm font-bold">Copyright &copy; Elixer Enterprise | {{ date('Y') }}</p>
+    <ul class="flex justify-center gap-8">
+        <li>
+            <a class="hover:text-brand-green-500 transition-colors duration-300"
+                href="https://instagram.com/purezenessence/" target="_blank">
+                <i class="fa-brands fa-instagram text-4xl"></i>
+            </a>
+        </li>
+        <li>
+            <a class="hover:text-brand-green-500 transition-colors duration-300" href="https://tiktok.com/@purezenessence"
+                target="_blank">
+                <i class="fa-brands fa-tiktok text-4xl"></i>
+            </a>
+        </li>
+    </ul>
+    <p class="text-center text-sm font-bold">Copyright &copy; Elixer Enterprise | {{ date('Y') }}</p>
 </footer>
