afbb9baf45db7f01b34e91ea195e62cd6f5dfaf5
Daniel Leith <elixerprince@gmail.com>
2025-07-23 22:09:45 -0500

    style: remove the problematic base classes

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,9 +1,9 @@
 @php
-  $classes = 'w-fit rounded-lg border-b-8 border-neutral-500 pb-2 text-center text-4xl font-bold';
+    $classes = 'w-fit rounded-lg border-b-8 pb-2 text-center text-4xl font-bold';
 @endphp
 
-<header class="mb-8">
-  <h2 {{ $attributes(['class' => $classes, 'id' => $id]) }}>
-    {{ $slot }}
-  </h2>
+<header>
+    <h2 {{ $attributes(['class' => $classes, 'id' => $id]) }}>
+        {{ $slot }}
+    </h2>
 </header>
