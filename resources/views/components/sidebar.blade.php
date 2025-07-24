609dc3e5f02bbecc61c17690477f70508c630b4e
Daniel Leith <elixerprince@gmail.com>
2025-07-23 22:02:08 -0500

    feat(sidebar): improve the styling and add a link to it

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,3 +1,12 @@
-<nav class="fixed top-0 bottom-0">
-    sidebar
+<nav class="fixed p-8 top-0 left-0 w-70 justify-center bg-black text-white bottom-0">
+    <a class="font-bold flex text-2xl flex-col items-center" href="#">
+        <x-brand-icon />
+        My Dashboard
+    </a>
+
+    <hr class="text-neutral-500/50 mt-4 mb-2 border-1">
+
+    <ul class="">
+        <li><x-navbar-link class="rounded-md" href="/dashboard/products" wire:navigate>All Products</x-navbar-link></li>
+    </ul>
 </nav>
