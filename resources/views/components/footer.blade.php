87ce1d9571f3f65aa40b4760e83fd8676e12dc37
Daniel Leith <elixerprince@gmail.com>
2025-07-24 11:23:02 -0500

    style(footer): add some social links styling to the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,14 @@
 <footer class="bg-brand-blue-950 flex flex-col gap-8 px-8 py-16 text-neutral-50">
     <ul class="flex justify-center gap-8">
-        <li>
+        <li
+            class="outline-3 outline-transparent hover:outline-brand-green-50 transition-colors duration-700 rounded-full outline-offset-8">
             <a class="hover:text-brand-green-500 transition-colors duration-300"
                 href="https://instagram.com/purezenessence/" target="_blank">
                 <i class="fa-brands fa-instagram text-4xl"></i>
             </a>
         </li>
-        <li>
+        <li
+            class="outline-3 outline-transparent hover:outline-brand-green-50 transition-colors duration-700 rounded-full outline-offset-8">
             <a class="hover:text-brand-green-500 transition-colors duration-300" href="https://tiktok.com/@purezenessence"
                 target="_blank">
                 <i class="fa-brands fa-tiktok text-4xl"></i>
