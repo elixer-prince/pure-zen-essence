d5f6a47b1ca89113145e0eb377dc8ccc0b02c9b3
Daniel Leith <elixerprince@gmail.com>
2025-07-24 13:40:46 -0500

    refactor: make the brand icon more flexible

--- resources/views/components/brand-icon.blade.php
+++ resources/views/components/brand-icon.blade.php
@@ -1,1 +1,5 @@
-<img class="w-12 animate-bounce" src="{{ asset('images/brand-icon.png') }}" alt="Brand Icon" />
+@php
+    $image = asset('images/brand-icon.png');
+@endphp
+
+<img {{ $attributes(['class' => 'w-12 animate-bounce', 'src' => $image, 'alt' => 'Brand Icon']) }} />
