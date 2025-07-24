452ceecafeedb3895928a07679548f62249264cb
Daniel Leith <elixerprince@gmail.com>
2025-07-24 05:32:16 -0500

    refactor: refactor the edit form to use the modular form components

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,19 +1,24 @@
 <x-layouts.dashboard>
-    <x-slot:page-title>Edit Product Name</x-slot:page-title>
+    <x-slot:page-title>Edit {{ $product->name }}</x-slot:page-title>
     <x-slot:page-description>...</x-slot:page-description>
 
     <div class="min-h-screen h-full border flex flex-col items-center justify-center">
         <livewire:product-card :$product />
 
         <x-forms.form class="mt-8 border" method="PATCH">
-            <div>
-                <x-forms.form-label for="title" />
-                <x-forms.form-input type="text" id="title" name="title" placeholder="Product Title" required />
-            </div>
-            <div>
-                <x-forms.form-label for="title" />
-                <x-forms.form-input id="title" name="title" placeholder="Product Title" />
-            </div>
+            <x-forms.field>
+                <x-forms.label for="title" />
+                <x-forms.input type="text" id="title" name="title" placeholder="Product Title" required />
+
+                <x-forms.error-message for="title" />
+            </x-forms.field>
+
+            <x-forms.field>
+                <x-forms.label for="title" />
+                <x-forms.input id="title" name="title" placeholder="Product Title" />
+
+                <x-forms.error-message for="title" />
+            </x-forms.field>
         </x-forms.form>
     </div>
 
