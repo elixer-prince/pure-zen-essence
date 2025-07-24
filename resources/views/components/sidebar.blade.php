cb9ac807dec570867fbecf1b81462cd3181df5b4
Daniel Leith <elixerprince@gmail.com>
2025-07-24 13:42:57 -0500

    style(sidebar): increase the spacing for better readability

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,14 +1,27 @@
 <nav class="fixed p-8 top-0 left-0 w-70 justify-center bg-black text-white bottom-0">
     <a class="font-bold flex text-2xl flex-col items-center" href="/dashboard">
-        <x-brand-icon />
+        <x-brand-icon class="my-4" />
         My Dashboard
     </a>
 
-    <hr class="text-neutral-500/50 mt-4 mb-2 border-1">
+    <hr class="text-neutral-500/50 mt-8 mb-4 border-1">
 
-    <ul class="">
-        <li><x-navbar-link class="rounded-md" href="/">Home</x-navbar-link></li>
-        <li><x-navbar-link class="rounded-md" href="/dashboard">Overview</x-navbar-link></li>
-        <li><x-navbar-link class="rounded-md" href="/dashboard/products">All Products</x-navbar-link></li>
+    <ul class="flex flex-col">
+        <li>
+            <x-navbar-link class="rounded-md" href="/">
+                <i class="fa-solid rotate-270 fa-arrow-up-from-bracket"></i>
+                Landing Page
+            </x-navbar-link>
+        </li>
+
+        <li>
+            <x-navbar-link class="rounded-md" href="/dashboard" wire:navigate>
+                Overview
+            </x-navbar-link>
+        </li>
+        <li>
+            <x-navbar-link class="rounded-md" href="/dashboard/products" wire:navigate>
+                All Products
+            </x-navbar-link>
+        </li>
     </ul>
-</nav>
