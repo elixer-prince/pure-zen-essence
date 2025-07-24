d86b881a63c2d1c67c8a40f9a029da95da40706c
Daniel Leith <elixerprince@gmail.com>
2025-07-23 23:12:03 -0500

    style: change the border colour of the about section image

--- resources/views/components/about-image.blade.php
+++ resources/views/components/about-image.blade.php
@@ -1,9 +1,5 @@
 <div
-  class="absolute top-0 left-1/2 flex aspect-square h-48 flex-1 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-3 border-neutral-50 bg-neutral-950 transition-all duration-500 lg:static lg:h-full lg:translate-x-0 lg:-translate-y-0"
->
-  <img
-    class="h-full w-full transition-transform duration-500 hover:scale-110"
-    src="{{ asset('images/placeholder-image.svg') }}"
-    alt="Owners Image"
-  />
+    class="absolute top-0 left-1/2 flex aspect-square h-48 flex-1 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-3 border-brand-blue-500 bg-neutral-950 transition-all duration-500 lg:static lg:h-full lg:translate-x-0 lg:-translate-y-0">
+    <img class="w-full h-full object-cover transition-transform duration-500 hover:scale-110"
+        src="{{ asset('images/placeholder-image.svg') }}" alt="Owners Image" />
 </div>
