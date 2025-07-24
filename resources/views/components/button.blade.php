f406f9e2f18e1c9d72e36c6105b01a9b35462509
Daniel Leith <elixerprince@gmail.com>
2025-07-24 11:22:13 -0500

    style: increase the tab spaces from 2 to four for increased readability

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,19 +1,19 @@
 @props(['type' => 'a'])
 
 @php
-  $classes = 'flex w-fit items-center gap-2 rounded-md px-4 py-2 font-bold transition-colors duration-300';
+    $classes = 'flex w-fit items-center gap-2 rounded-md px-4 py-2 font-bold transition-colors duration-300';
 @endphp
 
 @if ($type === 'a')
-  <a {{ $attributes(['class' => $classes, 'href' => $href ?? null]) }}>
-    {{ $slot }}
-  </a>
+    <a {{ $attributes(['class' => $classes, 'href' => $href ?? null]) }}>
+        {{ $slot }}
+    </a>
 @elseif ($type === 'button')
-  @php
-    $classes .= ' cursor-pointer';
-  @endphp
+    @php
+        $classes .= ' cursor-pointer';
+    @endphp
 
-  <button {{ $attributes(['class' => $classes]) }}>
-    {{ $slot }}
-  </button>
+    <button {{ $attributes(['class' => $classes]) }}>
+        {{ $slot }}
+    </button>
 @endif
