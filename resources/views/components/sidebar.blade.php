0835208e8da31ede33c4ad26ede900e97adb4df1
Daniel Leith <elixerprince@gmail.com>
2025-07-24 11:25:34 -0500

    feat(sidebar): add additional links to the sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
 <nav class="fixed p-8 top-0 left-0 w-70 justify-center bg-black text-white bottom-0">
-    <a class="font-bold flex text-2xl flex-col items-center" href="#">
+    <a class="font-bold flex text-2xl flex-col items-center" href="/dashboard">
         <x-brand-icon />
         My Dashboard
     </a>
@@ -7,6 +7,8 @@
     <hr class="text-neutral-500/50 mt-4 mb-2 border-1">
 
     <ul class="">
-        <li><x-navbar-link class="rounded-md" href="/dashboard/products" wire:navigate>All Products</x-navbar-link></li>
+        <li><x-navbar-link class="rounded-md" href="/">Home</x-navbar-link></li>
+        <li><x-navbar-link class="rounded-md" href="/dashboard">Overview</x-navbar-link></li>
+        <li><x-navbar-link class="rounded-md" href="/dashboard/products">All Products</x-navbar-link></li>
     </ul>
 </nav>
