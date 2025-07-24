56ed9b927f7ce64d22d164ab636082d006e91d12
Daniel Leith <elixerprince@gmail.com>
2025-07-23 19:07:07 -0500

    chore(product): add id to the sizes in the product factory, create a product seeder, and add it to the default db seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,31 +9,34 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
-  /**
-   * Define the model's default state.
-   *
-   * @return array<string, mixed>
-   */
-  public function definition(): array
-  {
-    return [
-      "image" => fake()->imageUrl(),
-      "title" => fake()->sentence(2),
-      "description" => fake()->paragraph(2),
-      "sizes" => [
-        [
-          "name" => fake()->word(),
-          "price" => random_int(50, 150),
-        ],
-        [
-          "name" => fake()->word(),
-          "price" => random_int(50, 150),
-        ],
-        [
-          "name" => fake()->word(),
-          "price" => random_int(50, 150),
-        ],
-      ],
-    ];
-  }
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "image" => fake()->imageUrl(),
+            "title" => fake()->sentence(2),
+            "description" => fake()->paragraph(2),
+            "sizes" => [
+                [
+                    "id" => "1",
+                    "name" => fake()->word(),
+                    "price" => random_int(50, 150),
+                ],
+                [
+                    "id" => "2",
+                    "name" => fake()->word(),
+                    "price" => random_int(50, 150),
+                ],
+                [
+                    "id" => "3",
+                    "name" => fake()->word(),
+                    "price" => random_int(50, 150),
+                ],
+            ],
+        ];
+    }
 }
