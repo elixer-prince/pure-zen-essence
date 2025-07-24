d7c7ecc23ef30621fa33c21226ab0af1a426adc5
Daniel Leith <elixerprince@gmail.com>
2025-07-24 13:48:55 -0500

    feat(navbar): add select none and improve code readability with whitespace

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,35 @@
 <nav
-    class="fixed shadow top-0 right-0 left-0 z-10 flex h-[var(--navbar-height)] items-center justify-between sm:bg-black/95 bg-black backdrop-blur-sm p-8">
+    class="fixed shadow top-0 right-0 select-none left-0 z-10 flex h-[var(--navbar-height)] items-center justify-between sm:bg-black/95 bg-black backdrop-blur-sm p-8">
     <div class="flex items-center gap-8">
         <x-brand-logo />
 
         <ul
             class="border-brand-green-50 hidden items-center overflow-hidden rounded-md border-2 lg:flex [&>li:first-child>a]:pl-4 [&>li:last-child>a]:pr-4">
-            <li><x-navbar-link href="#">HOME</x-navbar-link></li>
-            <li><x-navbar-link href="#about">ABOUT US</x-navbar-link></li>
-            <li><x-navbar-link href="#products">OUR PRODUCTS</x-navbar-link></li>
-            <li><x-navbar-link href="#testimonials">TESTIMONIALS</x-navbar-link></li>
-            <li><x-navbar-link href="#faqs">FAQs</x-navbar-link></li>
+            <li>
+                <x-navbar-link href="#">
+                    HOME
+                </x-navbar-link>
+            </li>
+            <li>
+                <x-navbar-link href="#about">
+                    ABOUT US
+                </x-navbar-link>
+            </li>
+            <li>
+                <x-navbar-link href="#products">
+                    OUR PRODUCTS
+                </x-navbar-link>
+            </li>
+            <li>
+                <x-navbar-link href="#testimonials">
+                    TESTIMONIALS
+                </x-navbar-link>
+            </li>
+            <li>
+                <x-navbar-link href="#faqs">
+                    FAQs
+                </x-navbar-link>
+            </li>
         </ul>
     </div>
 
@@ -27,8 +47,9 @@
         @endguest
 
         @auth
-            <button class="hover:text-red-300 text-red-50 transition-colors duration-300 font-bold">Log
-                Out</button>
+            <button class="hover:text-red-300 text-red-50 transition-colors duration-300 font-bold">
+                Log Out
+            </button>
 
             <x-button class="bg-brand-darkblue-900 text-brand-darkblue-50 hover:bg-brand-darkblue-800" href="/signup"
                 wire:navigate>
