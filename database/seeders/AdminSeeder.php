7a6fe084028db06dcfa94b52b8f59f23a8e7db35
Daniel Leith <elixerprince@gmail.com>
2025-07-23 19:12:36 -0500

    chore(admin): create the admin files

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Admin::factory(3)->create();
+    }
+}
