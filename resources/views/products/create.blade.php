991f30a9abc63865c1935b74da5b8bd732606e95
Daniel Leith <elixerprince@gmail.com>
2025-07-23 22:48:15 -0500

    feat: make an endpoint for creating products

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,42 @@
+<x-layouts.dashboard>
+    <x-slot:page-title>
+        Pure Zen Essence | Create Product
+    </x-slot:page-title>
+    <x-slot:page-description>
+        This is the page to create new products to be displayed.
+    </x-slot:page-description>
+
+    <div class="flex items-center p-8 min-h-screen justify-center">
+        <x-section id="create-product" class="rounded-md w-120 border-2 p-16 border-neutral-500"
+            aria-labelledby="create-product-header">
+            <x-section-heading id="create-product-header" class="border-brand-green-500 mb-6">
+                New Product
+            </x-section-heading>
+
+            <x-forms.form class="w-full" method="POST">
+                <x-forms.field>
+                    <x-forms.label for="title">Title</x-forms.label>
+                    <x-forms.input type="text" id="title" name="title" placeholder="Coconut Oil" required />
+                </x-forms.field>
+
+                <x-forms.field>
+                    <x-forms.label for="description">Description</x-forms.label>
+                    <x-forms.text-area id="description" name="description" placeholder="Add a description..."
+                        rows="3" required></x-forms.text-area>
+                </x-forms.field>
+
+                <x-forms.field>
+                    <x-forms.label for="price">Price</x-forms.label>
+                    <x-forms.input type="text" id="price" name="price" placeholder="$500 JMD" />
+                </x-forms.field>
+
+                <div>
+                    <button type="submit"
+                        class="bg-brand-green-500 transition-colors duration-300 cursor-pointer hover:bg-brand-green-600 mt-4 text-brand-green-50 font-bold py-2 rounded-md px-4">
+                        Create Product
+                    </button>
+                </div>
+            </x-forms.form>
+        </x-section>
+    </div>
+</x-layouts.dashboard>
