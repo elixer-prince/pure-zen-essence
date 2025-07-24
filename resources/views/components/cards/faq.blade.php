a8ebbba3634ba1edf2f1083385248d93052d766e
Daniel Leith <elixerprince@gmail.com>
2025-07-24 12:17:35 -0500

    style: add a custom toggle icon with animations

--- resources/views/components/cards/faq.blade.php
+++ resources/views/components/cards/faq.blade.php
@@ -1,9 +1,14 @@
 <details class="rounded-full open:rounded-xl group/faq bg-white">
     <summary
-        class="bg-brand-blue-500 group-open/faq:rounded-b-none group-open/faq:rounded-xl cursor-pointer rounded-full px-8 py-4 font-bold text-white">
+        class="bg-brand-blue-500 flex items-center gap-4 list-none group-open/faq:rounded-b-none group-open/faq:rounded-xl cursor-pointer rounded-full px-6 py-4 font-bold text-white">
+        <span class="text-2xl group-open/faq:rotate-90 transition-transform duration-500">
+            <i class="fa-regular fa-square-caret-right"></i>
+        </span>
+
         {{ $question }}
     </summary>
-    <p class="px-8 py-4">
+
+    <p class="px-8 ml-6 text-neutral-500 font-bold py-4">
         {{ $answer }}
     </p>
 </details>
