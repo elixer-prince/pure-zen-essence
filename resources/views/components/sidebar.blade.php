03edcda07444357a26fc806a91df2e308ef416f3
Daniel Leith <elixerprince@gmail.com>
2025-07-23 19:17:55 -0500

    feat: create a basic dashbboard view and sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,3 @@
+<nav class="fixed top-0 bottom-0">
+    sidebar
+</nav>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.dashboard>
+    <x-slot:page-title>
+        Pure Zen Essence | Dashboard
+    </x-slot:page-title>
+    <x-slot:page-description>
+        ...
+    </x-slot:page-description>
+
+    Welcome to the dashboard!
+</x-layouts.dashboard>
