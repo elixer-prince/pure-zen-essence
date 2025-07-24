0f031fa13dd3a495c02fcf9b99c15c750a12aa95
Daniel Leith <elixerprince@gmail.com>
2025-07-24 11:31:00 -0500

    style: change the styling of the cards

--- resources/views/components/cards/faq.blade.php
+++ resources/views/components/cards/faq.blade.php
@@ -1,6 +1,6 @@
-<details class="rounded-lg group/faq bg-white">
+<details class="rounded-full open:rounded-xl group/faq bg-white">
     <summary
-        class="bg-brand-blue-500 group-open/faq:rounded-b-none cursor-pointer rounded-lg px-8 py-4 font-bold text-white">
+        class="bg-brand-blue-500 group-open/faq:rounded-b-none group-open/faq:rounded-xl cursor-pointer rounded-full px-8 py-4 font-bold text-white">
         {{ $question }}
     </summary>
     <p class="px-8 py-4">
--- resources/views/components/cards/testimonial.blade.php
+++ resources/views/components/cards/testimonial.blade.php
@@ -1,6 +1,6 @@
 <div class="relative mt-12 flex rounded-lg border-2 border-neutral-500 bg-neutral-50 p-8 pt-18">
     <div
-        class="absolute top-0 left-1/2 aspect-square h-28 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-3 border-neutral-50 bg-neutral-500">
+        class="absolute top-0 left-1/2 aspect-square h-28 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-3 border-white bg-neutral-500">
         <img class="h-full w-full" src="{{ asset('images/placeholder-image.svg') }}" alt="Testimonial Image" />
     </div>
     <div>
