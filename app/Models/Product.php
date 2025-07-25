51b973a5aab15d65d8331373e7e716ad93a875f9
Daniel Leith <elixerprince@gmail.com>
2025-07-25 00:03:17 -0500

    docs: add comments to the product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * This is a product sold by the Pure Zen Essence business.
+ */
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ["image", "title", "description", "sizes"];
+
+    // Converts the JSON data (saved as a very large string) to an array
     protected $casts = [
         "sizes" => "array",
     ];
