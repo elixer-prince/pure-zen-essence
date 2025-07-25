d662a623c4f2c7e5ac36f98cb42a084ec1bc7b57
Daniel Leith <elixerprince@gmail.com>
2025-07-24 22:42:36 -0500

    fix: make the id attribute required on the section component

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -2,6 +2,6 @@
     $classes = 'flex scroll-mt-[var(--navbar-height)] flex-col items-center justify-center';
 @endphp
 
-<section {{ $attributes(['class' => $classes, 'id', 'aria-labelledby']) }}>
+<section {{ $attributes(['class' => $classes, 'id' => $id, 'aria-labelledby']) }}>
     {{ $slot }}
 </section>
