ba47fe5e3fb1b29470863676e53a8410dfb8f107
Daniel Leith <elixerprince@gmail.com>
2025-07-24 20:14:44 -0500

    style(footer): update the social link hover style in the footer to be more dynamic

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,21 @@
-<footer class="bg-brand-blue-950 flex flex-col gap-8 px-8 py-16 text-neutral-50">
-    <ul class="flex justify-center gap-8">
+<footer
+    class="bg-brand-blue-950 selection:bg-brand-blue-500! flex flex-col items-center gap-8 px-8 py-16 text-neutral-50">
+    <ul class="flex justify-center hover:gap-12 transition-all duration-700 w-fit gap-8">
         <li
-            class="outline-3 outline-transparent hover:outline-brand-green-50 transition-colors duration-700 rounded-full outline-offset-8">
+            class="outline-3 hover:-translate-y-1 hover:-translate-x-1 outline-transparent hover:outline-brand-green-50 transition-all duration-700 rounded-full outline-offset-16">
             <a class="hover:text-brand-green-500 transition-colors duration-300"
                 href="https://instagram.com/purezenessence/" target="_blank">
                 <i class="fa-brands fa-instagram text-4xl"></i>
             </a>
         </li>
         <li
-            class="outline-3 outline-transparent hover:outline-brand-green-50 transition-colors duration-700 rounded-full outline-offset-8">
+            class="outline-3 hover:-translate-y-1 hover:translate-x-1 outline-transparent hover:outline-brand-green-50 transition-all duration-700 rounded-full outline-offset-16">
             <a class="hover:text-brand-green-500 transition-colors duration-300" href="https://tiktok.com/@purezenessence"
                 target="_blank">
                 <i class="fa-brands fa-tiktok text-4xl"></i>
             </a>
         </li>
     </ul>
+
     <p class="text-center text-sm font-bold">Copyright &copy; Elixer Enterprise | {{ date('Y') }}</p>
 </footer>
