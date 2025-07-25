396d8fa3af1abb658da97d6a5eb29f11123e99a3
Daniel Leith <elixerprince@gmail.com>
2025-07-24 22:47:58 -0500

    feat(gsap): add a fade in animation to the hero section image

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,14 @@ gsap.from(".cta-content", {
     stagger: 0.25,
     opacity: 0,
 });
+
+// !BUG: #hero-mage animation not working
+
+/*
+    TODO: Research how GSAP animations work in greater detail using the official documentation
+*/
+
+gsap.from("#hero-image", {
+    opacity: 0,
+    duration: 2,
+});
