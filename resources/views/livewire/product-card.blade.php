149de6c91615b61d89d1432659d8b09c9874e7ae
Daniel Leith <elixerprince@gmail.com>
2025-07-25 02:45:04 -0500

    feat(product-card): display a delete button when on the card edit page

--- resources/views/livewire/product-card.blade.php
+++ resources/views/livewire/product-card.blade.php
@@ -29,6 +29,15 @@
                             Edit Card
                         </x-button>
                     @endunless
+
+                    @if (request()->is('dashboard/product/*/edit'))
+                        <x-forms.form action="/dashboard/product/{{ $product->id }}" method="DELETE">
+                            <button type="submit"
+                                class="border-2 px-4 py-2 rounded-md font-bold border-red-500 cursor-pointer group/button text-sm hover:text-red-50 text-red-500 hover:bg-red-500">
+                                Delete Card
+                            </button>
+                        </x-forms.form>
+                    @endif
                 </div>
             </div>
         </div>
