7756459d77f9b8fe4323640d0f5a668e0c69ed13
Daniel Leith <elixerprince@gmail.com>
2025-07-25 02:43:27 -0500

    feat(dashboard):

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.dashboard>
+    <x-slot:page-title>
+        Pure Zen Essence | Dashboard
+    </x-slot:page-title>
+    <x-slot:page-description>
+        This is the dashboard of the Pure Zen Essence website.
+    </x-slot:page-description>
+
+    <p>Welcome to the dashboard...</p>
+</x-layouts.dashboard>
