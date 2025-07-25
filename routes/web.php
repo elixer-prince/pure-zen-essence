1f509021e2c674f4dd0656a2639264ffb8d2a9a1
Daniel Leith <elixerprince@gmail.com>
2025-07-24 22:53:54 -0500

    feat(endpoints): add a bunch of messy endpoints to avoid getting 404 errors

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,74 @@ Route::get("/signup", function () {
     return view("auth.signup");
 });
 Route::post("/signup", function () {
-    dd("Signing up...");
+    dd(request()->all());
+});
+
+// Products endpoints
+
+//=========================================================================
+//--------------------DANGER ABOVE-----------------------------------------
+//=========================================================================
+
+// Dashboard Endpoints
+
+// dashboard index
+Route::get("/dashboard", function () {
+    $products = Product::all();
+
+    return view("dashboard.index", [
+        "products" => $products,
+    ]);
+});
+
+// dashboard products index
+Route::get("/dashboard/products", function () {
+    $products = Product::all();
+
+    return view("dashboard.products.index", [
+        "products" => $products,
+    ]);
+});
+
+// maybe I'll add a show endpoint
+
+// create product
+Route::get("/dashboard/products/create", function () {
+    return view("dashboard.products.create");
+});
+
+// post product
+Route::post("/dashboard/products", function () {
+    request()->validate([
+        "image" => ["required"],
+        "title" => ["required"],
+        "description" => ["required"],
+        "sizes" => [""],
+    ]);
+
+    Product::create([
+        "image" => "https://someurl.com/image/seed/{{ rand(1, 100000) }}",
+        "title" => request("title"),
+        "description" => request("description"),
+        "sizes" => ["name"],
+    ]);
+
+    return redirect("/dashboard");
+});
+
+// edit product
+Route::get("/dashboard/product/{product}/edit", function (Product $product) {
+    return view("dashboard.products.edit", [
+        "product" => $product,
+    ]);
+});
+
+// update product
+Route::patch("/dashboard/product/{product}", function (Product $product) {
+    dd("editing...");
+});
+
+// destroy product
+Route::delete("/dashboard/product/{product}", function (Product $product) {
+    dd("deleting...");
 });
