12baa19157e48cdadb37eb6dda8c75c3829fc047
Daniel Leith <elixerprince@gmail.com>
2025-07-24 22:38:48 -0500

    style(testimonial-card): remove the background colour of the image

--- resources/views/components/cards/testimonial.blade.php
+++ resources/views/components/cards/testimonial.blade.php
@@ -1,7 +1,7 @@
 <div
     class="relative mt-12 transition-all duration-500 hover:-translate-y-1.5 flex rounded-lg border-2 border-neutral-500 bg-neutral-50 p-8 pt-18">
     <div
-        class="absolute hover:rotate-360 duration-500 hover:scale-105 transition-transform top-0 left-1/2 aspect-square h-28 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-4 border-white bg-neutral-500">
+        class="absolute hover:rotate-360 duration-500 hover:scale-105 transition-transform top-0 left-1/2 aspect-square h-28 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-4 border-white">
         <img class="h-full w-full" src="https://picsum.photos/seed/{{ rand(1, 1000000) }}/300/300"
             alt="Testimonial Image" />
     </div>
