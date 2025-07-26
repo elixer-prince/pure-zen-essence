fb5291d554ee2a6c32d35efef015784f975e501e
Daniel Leith <elixerprince@gmail.com>
2025-07-26 17:06:39 -0500

    refactor(form): rename the forms directory to form for clarity

--- resources/views/components/forms/text-area.blade.php
+++
@@ -1,5 +0,0 @@
-@php
-    $classes = 'w-full resize-none rounded-md border-2 border-neutral-400 px-4 py-3 outline-none';
-@endphp
-
-<textarea {{ $attributes(['class' => $classes, 'id', 'name']) }}>{{ $slot }}</textarea>
