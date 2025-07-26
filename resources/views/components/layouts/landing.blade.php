bf534a4a1b348679eebfc74e59fc8d8beecbd24a
Daniel Leith <elixerprince@gmail.com>
2025-07-26 11:57:20 -0500

    feat(layout): create a landing page layout that is different from the layout for signed in users

--- resources/views/components/layouts/landing.blade.php
+++ resources/views/components/layouts/landing.blade.php
@@ -2,9 +2,9 @@
     <x-slot:page-title>{{ $pageTitle }}</x-slot:page-title>
     <x-slot:page-description>{{ $pageDescription }}</x-slot:page-description>
 
-    <x-navbar />
+    <x-hero-section />
 
-    <main class="mt-[var(--navbar-height)]">
+    <main>
         {{ $slot }}
     </main>
 
