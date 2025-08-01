d6feb30a200b716e4c4da72b7e4ac8b55f7ec303
Daniel Leith <elixerprince@gmail.com>
2025-08-01 14:42:55 -0500

    refactor: structure the routes so the relationships are clearer

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,65 @@
 <?php
 
 use App\Http\Controllers\Dashboard\ProductController as DashboardProductController;
+use App\Http\Controllers\Mail\InquiryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisteredUserController;
-use App\Http\Controllers\SessionController;
+use App\Http\Controllers\Session\UserController;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function () {
-    $products = Product::all();
+// TODO: 1. Research to see why suggested form test or initial text is tinier. It might be due to there being a pseudoclass.
 
-    return view("index", [
-        "products" => $products,
+// FIXME: 2. Make it so that the mobile navbar only show the / link on the / page
+
+Route::post("/mail", InquiryController::class);
+
+/*** GUEST ROUTES */
+Route::middleware("guest")->group(function () {
+    // Landing Page
+    Route::get("/", function () {
+        $products = Product::paginate(6);
+
+        return view("index", compact("products"));
+    });
+
+    // User Login
+    Route::controller(UserController::class)->group(function () {
+        Route::get("/login", "create")->name("login");
+        Route::post("/login", "store");
+    });
+
+    // User Registration
+    Route::controller(RegisteredUserController::class)->group(function () {
+        Route::get("/register", "create");
+        Route::post("/register", "store");
+    });
+});
+// ->can("index"); // TODO: Change to admin perms or some other policy
+
+/*** AUTHENTICATED USER ROUTES */
+Route::middleware("auth")->group(function () {
+    Route::delete("/session", [UserController::class, "destroy"]);
+
+    Route::resource("products", ProductController::class)->only([
+        "index",
+        "show",
     ]);
 });
 
-Route::get("/dashboard", function () {
-    $products = Product::all();
+/*** ADMIN ROUTES */
+Route::prefix("admin")
+    ->middleware(["auth", "admin"])
+    ->group(function () {
+        Route::get("/dashboard", function () {
+            $products = Product::paginate(6);
 
-    return view("dashboard.index", [
-        "products" => $products,
-    ]);
-});
+            return view("dashboard.index", compact("products"));
+        });
 
-Route::controller(SessionController::class)->group(function () {
-    Route::get("/login", "create");
-    Route::post("/login", "store");
-    Route::delete("/session", "destroy");
-});
-
-Route::controller(RegisteredUserController::class)->group(function () {
-    Route::get("/register", "create");
-    Route::post("/register", "store");
-});
-
-Route::resource("products", ProductController::class, [
-    "only" => ["index", "show"], // I might refactor this to a group in the future
-]);
-
-Route::resource("dashboard/products", DashboardProductController::class);
+        Route::resource(
+            "/dashboard/products",
+            DashboardProductController::class,
+        );
+    });
