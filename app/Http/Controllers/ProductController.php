8b6c44be2dc82416b4757492d1757101fe488fc2
Daniel Leith <elixerprince@gmail.com>
2025-08-01 14:37:20 -0500

    feat(product-controller): comment the code and pass data to the vews

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,13 +6,21 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
+    /**
+     * Display a listing of the products.
+     */
     public function index()
     {
-        return view("products.index");
+        $products = Product::paginate(6);
+
+        return view("products.index", compact("products"));
     }
 
+    /**
+     * Show a specific product.
+     */
     public function show(Product $product)
     {
-        return view("products.show");
+        return view("products.show", compact("product"));
     }
 }
