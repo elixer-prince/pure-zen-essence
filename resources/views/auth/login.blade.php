6a92f3d3c7764732dce7c9c4c35c19f6a855df49
Daniel Leith <elixerprince@gmail.com>
2025-08-01 14:06:10 -0500

    fix(login-form): use the correct component names and add error messages/old data

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <main class="min-h-full">
         <x-section id="login" class="flex px-8 py-16 sm:px-16 items-center justify-center"
             aria-labelledby="login-header">
-            <x-forms.form class="border-2 max-w-120 w-full border-neutral-500 rounded-lg p-8 sm:p-16" action="/login"
+            <x-form class="border-2 max-w-120 w-full border-neutral-500 rounded-lg p-8 sm:p-16" action="/login"
                 method="POST">
                 <div class="flex flex-col items-center">
                     <x-brand-icon />
@@ -16,28 +16,30 @@
                 </div>
 
                 <div class="flex flex-col mb-2 gap-4">
-                    <x-forms.field>
-                        <x-forms.label for="email">Email</x-forms.label>
-                        <x-forms.input id="email" type="email" name="email" placeholder="Your Email"
+                    <x-form.field>
+                        <x-form.label for="email">Email</x-form.label>
+                        <x-form.input id="email" type="email" name="email" placeholder="Your Email"
                             value="{{ old('email') }}" required />
-                    </x-forms.field>
 
-                    <x-forms.field>
-                        <x-forms.label for="password">Password</x-forms.label>
-                        <x-forms.input id="password" type="password" name="password" placeholder="Your Password"
+                        <x-form.error-message for="email" />
+                    </x-form.field>
+
+                    <x-form.field>
+                        <x-form.label for="password">Password</x-form.label>
+                        <x-form.input id="password" type="password" name="password" placeholder="Your Password"
                             required />
-                    </x-forms.field>
+                    </x-form.field>
 
                     <a href="/register" class="text-sm text-neutral-700 -mt-1 font-bold hover:underline" wire:navigate>
                         Don't have an account? Create one!
                     </a>
                 </div>
 
-                <x-button class="bg-brand-green-500 text-brand-green-50 hover:bg-brand-green-600" type="button">
+                <x-button class="bg-brand-green-500 text-brand-green-50 hover:bg-brand-green-600" type="submit">
                     Login
                     <i class="fa-solid fa-user"></i>
                 </x-button>
-            </x-forms.form>
+            </x-form>
         </x-section>
     </main>
 </x-layouts.app>
