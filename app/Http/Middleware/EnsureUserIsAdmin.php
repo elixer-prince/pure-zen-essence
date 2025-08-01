5cc3759d6b1d606c059ddcd688bac45540a9d5b7
Daniel Leith <elixerprince@gmail.com>
2025-08-01 14:38:23 -0500

    feat: create a basic admin middleware and product policy

---
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Check if the user is an admin
+        if (true) {
+            return redirect("/products")->with(
+                "error",
+                "You do not have admin access.",
+            );
+        }
+
+        // return $next($request);
+    }
+}
