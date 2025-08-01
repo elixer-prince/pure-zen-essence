007fe681aeaf8fafbd38e8fd443ddf1feb779ea1
Daniel Leith <elixerprince@gmail.com>
2025-08-01 01:38:15 -0500

    refactor: make the brand logo file more readable

--- resources/views/components/brand-logo.blade.php
+++ resources/views/components/brand-logo.blade.php
@@ -1,5 +1,10 @@
-<a class="flex items-center gap-2" href="/">
+@php
+    $classes = 'flex items-center gap-2';
+@endphp
+
+<a {{ $attributes(['class' => $classes, 'href' => '/']) }}>
     <x-brand-icon />
+
     <span
         class="text-brand-green-50 hidden text-2xl transition-transform duration-500 hover:-translate-y-0.5 font-bold sm:flex">
         Pure Zen Essence
