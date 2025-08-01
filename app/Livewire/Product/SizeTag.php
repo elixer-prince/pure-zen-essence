05f4dfc8cf5cb3ba155ef36d928072cfb187f0a1
Daniel Leith <elixerprince@gmail.com>
2025-08-01 14:40:59 -0500

    feat: attempt to fix the size tag and add comments to the code

--- app/Livewire/Product/SizeTag.php
+++ app/Livewire/Product/SizeTag.php
@@ -6,39 +6,55 @@ use App\Models\Product;
 use Livewire\Component;
 
 /**
- * Represents a size tag component for a product.
+ * This is a size tag for a product, of which there can be many.
  *
- * Handles the display and selection of size-specific data,
- * including emitting price update events on selection.
+ * This component handles the display and manipulation
+ * of size-specific data, including dispatching a
+ * "price-updated" event on click.
  */
 class SizeTag extends Component
 {
-    /** The product component passed from the product card. */
+    /**
+     * This is passed from the product card because it is used
+     * to scope each operation to a specific product card.
+     * */
     public Product $product;
-
-    /** The size array passed from the product card. */
+    /**
+     * This is passed from the product card and is a specific
+     * size used to get the size properties to display/modify.
+     * */
     public array $size;
+    /**
+     * This is the id belonging to/scoped to a specific
+     * product size tag.
+     */
+    public int $sizeId;
+    /**
+     * This is a copy passed from the product card and is the id of
+     * the currently selected size.
+     * */
+    public int $currentSizeId;
 
-    /** The selected state of each individual tag. */
-    public bool $selected = false;
+    public function mount(Product $product, array $size): void
+    {
+        $this->product = $product;
+        $this->sizeId = $size["id"];
+    }
 
     /**
-     * Dispatches a "price-updated" event each time the size tag is clicked.
+     * Dispatches a "price-updated" event each time the size tag
+     * is clicked.
      *
-     * @param (int) $price
-     * @param (Product) $product
+     * @param (int) $price - The price of the currently selected tag.
+     * @param (Product) $product - The specific product that each
+     * tag belongs to.
      */
-    public function setPrice(
-        int $price,
-        Product $product,
-        int $size,
-        bool $selected,
-    ): void {
-        $this->dispatch(
-            "price-updated.{$product["id"]}",
-            $price,
-            $size,
-            $selected,
-        );
+    public function setPrice(int $sizeId, int $price, Product $product)
+    {
+        if ($this->currentSizeId === $sizeId) {
+            return;
+        }
+
+        $this->dispatch("price-updated.{$product->id}", $price, $sizeId);
     }
 }
