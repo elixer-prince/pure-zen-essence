193d8c7eec38e67baaf53a6fa574900e01fed131
Daniel Leith <elixerprince@gmail.com>
2025-08-01 00:56:20 -0500

    feat(products.index): add an empty section for displaying products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,5 +4,11 @@
         Here is a list of all the products sold by Pure Zen Essence.
     </x-slot:page-description>
 
-    <p>View all products here...</p>
+    <x-section id="products" class="py-16 px-8" aria-labelledby="products-header">
+        <x-section-heading id="products-header" class="border-b-brand-green-500 mb-8">
+            All Products
+        </x-section-heading>
+
+        <p>View all products here...</p>
+    </x-section>
 </x-layouts.normal>
