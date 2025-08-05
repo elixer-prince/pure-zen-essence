89ccc306459a17cd2921f3e1269a793597f18eff
Daniel Leith <elixerprince@gmail.com>
2025-08-05 10:37:06 -0500

    refactor(navbar): create dedicated components for modularity

--- resources/views/components/navbar/footer.blade.php
+++ resources/views/components/navbar/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="text-center text-neutral-50/75 text-sm mt-auto">
+    <div class="text-center pt-auto text-neutral-50/75 text-sm mt-auto">
         <p>Copyright &copy; {{ date('Y') }} Pure Zen Essence. </p>
         <p>All rights reserved.</p>
     </div>
