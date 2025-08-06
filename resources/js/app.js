217d75c700ba51e9e985a970ccdd0d6eeae91385
Daniel Leith <elixerprince@gmail.com>
2025-08-06 00:41:48 -0500

    fix: make app.js wait for all content to be loaded to avoid calling missing elements

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,22 @@
 import "./bootstrap";
 import gsap from "gsap";
 
-gsap.from(".cta-content", {
-    y: 300,
-    duration: 1.25,
-    stagger: 0.25,
-    opacity: 0,
-});
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.from(".cta-content", {
+        y: 300,
+        duration: 1.25,
+        stagger: 0.25,
+        opacity: 0,
+    });
 
-// !BUG: #hero-mage animation not working
+    // !BUG: #hero-image animation not working
 
-/*
-    TODO: Research how GSAP animations work in greater detail using the official documentation
-*/
+    /*
+        TODO: Research how GSAP animations work in greater detail using the official documentation
+    */
 
-gsap.from("#hero-image", {
-    opacity: 0,
-    duration: 2,
+    gsap.from("#hero-image", {
+        opacity: 0,
+        duration: 2,
+    });
 });
