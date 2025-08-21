a2164545aaf107807a31f269dbf4288c625729d7
Daniel Leith <elixerprince@gmail.com>
2025-08-21 14:54:45 -0500

    style: aff more breathing rooma nd change the card colour

--- resources/views/components/cards/about.blade.php
+++ resources/views/components/cards/about.blade.php
@@ -1,15 +1,15 @@
 @php
     $classes =
-        "group border-brand-green-500 rounded-lg border-l-6 bg-neutral-900 p-8 transition-transform duration-500 hover:-translate-y-1";
+        "group border-brand-green-500 bg-brand-green-900 rounded-lg border-l-6 p-8 transition-transform duration-500 hover:-translate-y-1";
 @endphp
 
 <div {{ $attributes(["class" => $classes]) }}>
     <h3
-        class="group-hover:text-brand-blue-50 mb-2 text-2xl font-bold text-neutral-300 transition-colors duration-500"
+        class="text-h4 mb-2 font-bold text-neutral-50 transition-colors duration-500"
     >
         {{ $heading }}
     </h3>
-    <p class="text-neutral-400">
+    <p class="text-brand-green-100">
         {{ $slot }}
     </p>
 </div>
