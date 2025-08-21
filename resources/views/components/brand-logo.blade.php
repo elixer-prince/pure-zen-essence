383a56da7f1a64c557dd26cb57957017788de7ad
Daniel Leith <elixerprince@gmail.com>
2025-08-21 14:31:39 -0500

    style(brand-logo): improve the logo styling and add a title attribute

--- resources/views/components/brand-logo.blade.php
+++ resources/views/components/brand-logo.blade.php
@@ -1,12 +1,14 @@
 @php
-    $classes = "flex items-center gap-2";
+    $classes = "group/logo flex items-center gap-2";
 @endphp
 
-<a {{ $attributes(["class" => $classes, "href" => "/"]) }}>
+<a
+    {{ $attributes(["class" => $classes, "href" => $href, "title" => "Pure Zen Essence"]) }}
+>
     <x-brand-icon />
 
     <span
-        class="text-brand-green-50 hidden text-2xl font-bold transition-transform duration-500 hover:-translate-y-0.5 sm:flex"
+        class="text-brand-green-600 text-lg-subtitle hidden font-bold italic transition-transform duration-500 group-hover/logo:-translate-y-0.5 sm:flex"
     >
         Pure Zen Essence
     </span>
