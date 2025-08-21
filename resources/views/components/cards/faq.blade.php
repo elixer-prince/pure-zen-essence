fb37f0c6e8c4741e483f86e2641cbcd0eaeb3312
Daniel Leith <elixerprince@gmail.com>
2025-08-21 17:59:16 -0500

    style(faq): improve the styling of the faq section and cards

--- resources/views/components/cards/faq.blade.php
+++ resources/views/components/cards/faq.blade.php
@@ -1,19 +1,18 @@
 <details
-    class="group/faq rounded-full rounded-l-none bg-white odd:mr-16 even:ml-16 even:bg-red-500 open:rounded-r-xl"
+    class="group/faq bg-brand-blue-50 text-brand-blue-950 rounded-t-full rounded-b-full open:rounded-t-xl open:rounded-b-xl"
 >
     <summary
-        class="bg-brand-blue-500 flex cursor-pointer list-none items-center gap-4 rounded-full rounded-l-none px-6 py-4 font-bold text-white group-open/faq:rounded-r-xl group-open/faq:rounded-b-none"
+        class="bg-brand-blue-500 text-brand-blue-50 flex cursor-pointer items-center gap-4 rounded-t-full rounded-b-full px-6 py-4 font-bold group-open/faq:rounded-t-xl group-open/faq:rounded-b-none"
     >
         <span
             class="text-2xl transition-transform duration-500 group-open/faq:rotate-90"
         >
             <i class="fa-regular fa-square-caret-right"></i>
         </span>
-
         {{ $question }}
     </summary>
 
-    <p class="ml-6 px-8 py-4 font-bold text-neutral-500">
+    <p class="px-6 py-4 font-bold text-neutral-500">
         {{ $answer }}
     </p>
 </details>
--- resources/views/components/sections/faqs.blade.php
+++ resources/views/components/sections/faqs.blade.php
@@ -1,33 +1,38 @@
 <x-section
     id="faqs"
-    class="bg-brand-darkblue-950 px-0 py-16 sm:px-16"
+    class="bg-brand-darkblue-950 px-0 py-16 md:px-16"
     aria-labelledby="faqs-header"
 >
-    <x-section-heading
-        id="faqs-header"
-        class="border-b-brand-green-500 text-brand-darkblue-50 mb-8"
-    >
+    <x-section-heading id="faqs-header" class="text-brand-darkblue-50 mb-8">
         FAQs
     </x-section-heading>
 
-    <x-grid class="w-full md:grid-cols-2">
-        <div>
+    <ul
+        class="grid w-full gap-8 lg:grid-cols-2 max-md:[&>li]:odd:[&_*]:rounded-tl-none max-md:[&>li]:odd:[&_*]:rounded-bl-none max-md:[&>li]:even:[&_*]:rounded-tr-none max-md:[&>li]:even:[&_*]:rounded-br-none max-md:[&>li]:odd:[&>details]:mr-16 max-md:[&>li]:even:[&>details]:ml-16"
+    >
+        <li>
             <x-cards.faq>
-                <x-slot name="question">Is the world flat?</x-slot>
-                <x-slot name="answer">Yes it is</x-slot>
+                <x-slot name="question">Do you have this product?</x-slot>
+                <x-slot name="answer">Yes we do!</x-slot>
             </x-cards.faq>
-        </div>
-        <div>
+        </li>
+        <li>
             <x-cards.faq>
-                <x-slot name="question">Is the world flat?</x-slot>
-                <x-slot name="answer">Yes it is</x-slot>
+                <x-slot name="question">
+                    What type of business do you operate?
+                </x-slot>
+                <x-slot name="answer">
+                    We are a Natural Products business, founded by the Cartys!
+                </x-slot>
             </x-cards.faq>
-        </div>
-        <div>
+        </li>
+        <li>
             <x-cards.faq>
-                <x-slot name="question">Is the world flat?</x-slot>
-                <x-slot name="answer">Yes it is</x-slot>
+                <x-slot name="question">What do you sell?</x-slot>
+                <x-slot name="answer">
+                    We sell Natural Oils and Beauty Bar Soaps!
+                </x-slot>
             </x-cards.faq>
-        </div>
-    </x-grid>
+        </li>
+    </ul>
 </x-section>
