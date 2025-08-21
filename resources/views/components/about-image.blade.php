47fdf3a5a4a27ea6e252fa85d0e5bcecb302c8c2
Daniel Leith <elixerprince@gmail.com>
2025-08-21 14:30:14 -0500

    style(about-image): change the placeholder image and element styling

--- resources/views/components/about-image.blade.php
+++ resources/views/components/about-image.blade.php
@@ -1,12 +1,12 @@
 @php
     $classes =
-        "lg:border-brand-blue-500 absolute top-0 left-1/2 flex aspect-square h-48 flex-1 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-4 border-white bg-neutral-900 transition-all duration-500 lg:static lg:h-full lg:translate-x-0 lg:-translate-y-0";
+        "bg-brand-green-900 absolute top-0 left-1/2 flex aspect-square h-48 flex-1 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full border-4 border-neutral-50 transition-all duration-500 lg:static lg:h-full lg:translate-x-0 lg:-translate-y-0";
 @endphp
 
 <div {{ $attributes(["class" => $classes]) }}>
     <img
         class="h-full w-full object-cover transition-transform duration-500 hover:scale-110"
-        src="https://picsum.photos/seed/{{ rand(1, 100000) }}/300/300"
+        src="{{ asset("images/placeholder-image.svg") }}"
         alt="Owners Image"
     />
 </div>
